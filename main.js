c281a378cb664bfd06f39d08857bde24f7219f77
Eduardo Olsen <142837895+OzenPpk@users.noreply.github.com>
2024-10-02 12:26:21 -0300

    aaa

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
-// alert('deu certo.. Wagner 39, Luiz 24')
-// function soma(num1,num2) {
-//     return num1+num2
-// }
-// function multi(num1,num2){
-//     return num1*num2
-// }
-// function areaquadrado(lado){
-//     return lado**2
-// }
-// let media= function(num1, num2, num3){
-//     return (num1+num2+num3)/3
-// }
-// let nome=prompt("Qual Média vc quer?")
-// document.write(media)
-// parseInt(prompt(media))
-// console.log(soma(3,4))
-// console.log(multi(4,5))
-// console.log(areaquadrado(5))
-// console.log(media(4,4,4))
+// // alert('deu certo.. Wagner 39, Luiz 24')
+// // function soma(num1,num2) {
+// //     return num1+num2
+// // }
+// // function multi(num1,num2){
+// //     return num1*num2
+// // }
+// // function areaquadrado(lado){
+// //     return lado**2
+// // }
+// // let media= function(num1, num2, num3){
+// //     return (num1+num2+num3)/3
+// // }
+// // let nome=prompt("Qual Média vc quer?")
+// // document.write(media)
+// // parseInt(prompt(media))
+// // console.log(soma(3,4))
+// // console.log(multi(4,5))
+// // console.log(areaquadrado(5))
+// // console.log(media(4,4,4))
 
 
-let estados=['ac','al','ap','am','ba','ce','df','es','go','ma','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to']
-let i=0;
-while(i<27){
-    console.log(i);
-    console.log(estados[i]);
-    i=i++;
-}
+// let estados=['ac','al','ap','am','ba','ce','df','es','go','ma','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to']
+// let i=0;
+// while(i<27){
+//     console.log(i);
+//     console.log(estados[i]);
+//     i=i++;
+// }
 
-for (let i = 0; i < estados.length; i++) {
-    onsole.log(i);
-    console.log(estados[i]);
+// for (let i = 0; i < estados.length; i++) {
+//     onsole.log(i);
+//     console.log(estados[i]);
     
+
+}
+let idade=17;
+if(idade>17){
+    console.log('pode dirgir')
+}
+else }{console.log('ilegal dirigir')
+
 }
