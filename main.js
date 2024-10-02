3a28a838773541df9c39e38403b9e27eb138bac0
Eduardo Olsen <142837895+OzenPpk@users.noreply.github.com>
2024-10-02 12:13:08 -0300

    aaaaa

--- main.js
+++ main.js
@@ -20,7 +20,7 @@
 // console.log(media(4,4,4))
 
 
-let estados=[ 'ac','al','ap','am','ba','ce','df','es','go','ma','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to']
+let estados=['ac','al','ap','am','ba','ce','df','es','go','ma','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to']
 let i=0;
 while(i<27){
     console.log(i);
