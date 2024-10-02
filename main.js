374a8e3535d08781fe2b93ffbfa560152faf286d
Eduardo Olsen <142837895+OzenPpk@users.noreply.github.com>
2024-10-02 11:39:52 -0300

    aaa

---
+++ main.js
@@ -0,0 +1,22 @@
+// alert('deu certo.. Wagner 39, Luiz 24')
+// function soma(num1,num2) {
+//     return num1+num2
+// }
+// function multi(num1,num2){
+//     return num1*num2
+// }
+// function areaquadrado(lado){
+//     return lado**2
+// }
+// let media= function(num1, num2, num3){
+//     return (num1+num2+num3)/3
+// }
+// let nome=prompt("Qual Média vc quer?")
+// document.write(media)
+// parseInt(prompt(media))
+// console.log(soma(3,4))
+// console.log(multi(4,5))
+// console.log(areaquadrado(5))
+// console.log(media(4,4,4))
+
+let estados=['ac','al','ap','am','ba','ce','df','es','go','ma','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to']
