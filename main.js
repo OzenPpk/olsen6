15f815b4c5e1126810caf8b064b8d3bd82fb18ab
Eduardo Olsen <142837895+OzenPpk@users.noreply.github.com>
2024-10-02 12:06:28 -0300

    aaaa

--- main.js
+++ main.js
@@ -22,3 +22,8 @@
 
 let estados=['ac','al','ap','am','ba','ce','df','es','go','ma','mt','ms','mg','pa','pb','pr','pe','pi','rj','rn','rs','ro','rr','sc','sp','se','to']
 let i=0;
+while(i<27){
+    console.log(i);
+    console.log(estados[i]);
+    i=i+1;
+}
